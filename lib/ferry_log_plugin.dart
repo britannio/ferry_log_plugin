58ffb2f2f9a29155ba14de2c30c4266bd537fc76
Britannio <britanniojarrett@gmail.com>
2020-09-09 20:44:07 +0100

    implement plugin

--- lib/ferry_log_plugin.dart
+++ lib/ferry_log_plugin.dart
@@ -1,7 +1,37 @@
 library ferry_log_plugin;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+import 'package:ferry/ferry.dart';
+import 'package:ferry/plugins.dart';
+
+typedef RequestCallback = Function(OperationRequest);
+typedef ResponseCallback = Function(OperationResponse);
+
+class FerryLogPlugin extends Plugin {
+  FerryLogPlugin({@required this.onRequest, @required this.onResponse});
+  final RequestCallback onRequest;
+  final ResponseCallback onResponse;
+
+  @override
+  StreamTransformer<OperationRequest<TData, TVars>,
+      OperationRequest<TData, TVars>> buildRequestTransformer<TData, TVars>() {
+    return StreamTransformer.fromBind(
+        (Stream<OperationRequest<TData, TVars>> requestStream) {
+      requestStream.forEach((r) => onRequest(r));
+      return requestStream;
+    });
+  }
+
+  @override
+  StreamTransformer<OperationResponse<TData, TVars>,
+          OperationResponse<TData, TVars>>
+      buildResponseTransformer<TData, TVars>() {
+    return StreamTransformer.fromBind(
+        (Stream<OperationResponse<TData, TVars>> responseStream) {
+      responseStream.forEach(onResponse);
+      return responseStream;
+    });
+  }
 }
