4447ec1f858fc8ac36097195acef32ba9ac762e8
Britannio <britanniojarrett@gmail.com>
2020-10-16 11:14:54 +0100

    update ferry

--- lib/ferry_log_plugin.dart
+++ lib/ferry_log_plugin.dart
@@ -4,34 +4,24 @@ import 'dart:async';
 
 import 'package:meta/meta.dart';
 import 'package:ferry/ferry.dart';
-import 'package:ferry/plugins.dart';
 
-typedef RequestCallback = Function(OperationRequest);
-typedef ResponseCallback = Function(OperationResponse);
+typedef RequestCallback = void Function(OperationRequest);
+typedef ResponseCallback = void Function(OperationResponse);
 
-class FerryLogPlugin extends Plugin {
+class FerryLogPlugin extends TypedLink {
   FerryLogPlugin({@required this.onRequest, @required this.onResponse});
   final RequestCallback onRequest;
   final ResponseCallback onResponse;
 
   @override
-  StreamTransformer<OperationRequest<TData, TVars>,
-      OperationRequest<TData, TVars>> buildRequestTransformer<TData, TVars>() {
-    return StreamTransformer.fromBind(
-        (Stream<OperationRequest<TData, TVars>> requestStream) {
-      requestStream.forEach((r) => onRequest(r));
-      return requestStream;
-    });
-  }
-
-  @override
-  StreamTransformer<OperationResponse<TData, TVars>,
-          OperationResponse<TData, TVars>>
-      buildResponseTransformer<TData, TVars>() {
-    return StreamTransformer.fromBind(
-        (Stream<OperationResponse<TData, TVars>> responseStream) {
-      responseStream.forEach(onResponse);
-      return responseStream;
-    });
+  Stream<OperationResponse<TData, TVars>> request<TData, TVars>(
+    OperationRequest<TData, TVars> request, [
+    forward,
+  ]) async* {
+    onRequest(request);
+    await for (final response in forward(request)) {
+      onResponse(response);
+      yield response;
+    }
   }
 }
